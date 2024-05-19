7c5602b6185e114d16a31ca4d6eb92dedf638f2e
calebgrae09 <103107959+calebgrae09@users.noreply.github.com>
2024-05-19 12:50:57 +0000

    Add files via upload

---
+++ userInfo.js
@@ -0,0 +1,13 @@
+// userInfo.js
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => {
+        return {
+            id: index + 1,
+            originalName: name,
+            modifiedName: modifiedNames[index]
+        };
+    });
+}
+
+module.exports = { createUserProfiles };
