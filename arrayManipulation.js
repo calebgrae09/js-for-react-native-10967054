fd445556c15401f0e88d8929402877452c192ccb
calebgrae09 <103107959+calebgrae09@users.noreply.github.com>
2024-05-19 12:41:23 +0000

    Add files via upload

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,7 +1,5 @@
 // arrayManipulation.js
 
-// arrayManipulation.js
-
 function processArray(numbers) {
     return numbers.map(num => {
         if (num % 2 === 0) {
@@ -12,4 +10,13 @@ function processArray(numbers) {
     });
 }
 
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase(); // Capitalize if the number is even
+        } else {
+            return str.toLowerCase(); // Convert to lowercase if the number is odd
+        }
+    });
+}
 module.exports = { processArray };
